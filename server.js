84f42990b6375cdf4319fbf055787438fdf2627d
Super-cloud0 <brobropups@gmail.com>
2025-08-28 15:15:30 +0500

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware (промежуточное ПО)
-app.use(cors());
+app.use(cors({ origin: 'https://zayavleniya-site.vercel.app' }));
 app.use(express.json());
 
 // Подключение к базе данных MongoDB
